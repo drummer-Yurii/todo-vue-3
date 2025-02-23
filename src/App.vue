ce81a45a45fda232b45acddc65baa2adfaa24f79
Yurii Rybachok <drummer2009@ukr.net>
2025-02-23 11:13:10 +0200

    add form & logic for create todos

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,31 @@
 <script setup>
+import { ref } from 'vue';
+
+const newTodo = ref('');
+const todosData = JSON.parse(localStorage.getItem('todos')) || [];
+const todos = ref(todosData);
+
+const addTodo = () => {
+  if (newTodo.value) {
+    todos.value.push({
+      done: false,
+      content: newTodo.value,
+    });
+    newTodo.value = '';
+  }
+};
 </script>
 
 <template>
-  <div>
-   
-  </div>
+  <h1>Todo App</h1>
+  <form @submit.prevent="addTodo">
+    <label>New todo</label>
+    <input type="text" v-model="newTodo" />
+    <button type="submit">Add new todo</button>
+  </form>
+  <h2>Todo List</h2>
+  {{ todos }} 
+  <ul>
+    <li></li>
+  </ul>
 </template>
